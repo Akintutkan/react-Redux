6ff7b8de63c809ec7cf39c7403670c30918ec578
Akin94 <akin.tutkan@gmail.com>
2022-12-18 17:42:48 +0300

    Redux Example

--- redux-toolkit/src/App.js
+++ redux-toolkit/src/App.js
@@ -3,17 +3,26 @@ import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
 import Navbar from "./components/Navbar"
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals,getCartItems } from "./features/cart/cartSlice";
 
 
 
 function App() {
-  const {cartItems} = useSelector((store) => store.cart)
+  const {cartItems, isLoading} = useSelector((store) => store.cart)
   const {isOpen} = useSelector((store) => store.modal)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(calculateTotals())
   },[cartItems])
+  useEffect(() => {dispatch(getCartItems())
+},[])
+if(isLoading) {
+  return(
+    <div className="Loading">
+      <h4>Loading</h4>
+    </div>
+  )
+}
   return (
     <main>
       {isOpen && <Modal />}
--- redux-toolkit/src/components/Modal.js
+++ redux-toolkit/src/components/Modal.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { clearCart } from '../features/cart/cartSlice'
+import { closeModal } from '../features/modal/modalSlice'
 
 const Modal = () => {
+    const dispatch = useDispatch()
   return (
     <aside className="modal-container">
         <div className='modal'>
             <h4>Bütün ürünleri Kaldır?</h4>
             <div  className='btn-container'>
-                <button type="button" className='btn confirm-btn'>Onayla</button> 
-                <button type="button" className='btn confirm-btn'>Reddet</button> 
+                <button type="button" className='btn confirm-btn' onClick={()=>{
+                    dispatch(clearCart())
+                    dispatch(closeModal())
+                }}>
+                    Onayla</button> 
+                <button type="button" 
+                className='btn confirm-btn' 
+                onClick= {() => dispatch(closeModal())}>Reddet</button> 
             </div>
         </div>
     </aside>
